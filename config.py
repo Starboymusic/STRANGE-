a9e99d036d5511c296f998ee8113015b5cc4a1ec
Starboymusic <eozjaiix>
2024-09-04 15:01:20 +0530

    update

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ API_HASH = getenv("API_HASH")
 # ------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN")
 # -------------------------------------------------------
-OWNER_USERNAME = getenv("OWNER_USERNAME","SHIVANSHDEVS")
+OWNER_USERNAME = getenv("OWNER_USERNAME","Star_Boy_96")
 # --------------------------------------------------------
 BOT_USERNAME = getenv("BOT_USERNAME" , "Itz_SapnaMusicbot")
 # --------------------------------------------------------
